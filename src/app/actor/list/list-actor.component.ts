774b5d94a38cdc792b1ef998f35a3daee162faa7
Joymangul Jensen Selwyn <jensen.joymangul@etu.univ-lyon1.fr>
2017-10-24 11:31:10 +0200

    Resolve rules that does not follow the tslint.json

--- src/app/actor/list/list-actor.component.ts
+++ src/app/actor/list/list-actor.component.ts
@@ -1,21 +1,21 @@
-import {Component, OnInit} from '@angular/core'
+import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs/Rx';
 import { HttpClient } from '@angular/common/http';
-import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/map';
 import {Actor} from '../actor';
 
 
 @Component({
     selector: 'list-actor-root',
-    templateUrl:'./list-actor.component.html',
+    templateUrl: './list-actor.component.html',
     styleUrls: ['./list-actor.component.css'],
     providers: []
 })
 
 
-export class ListActorComponent implements OnInit{
-    actors : Actor[];
-  
+export class ListActorComponent implements OnInit {
+    actors: Actor[];
+
     constructor(private http: HttpClient) {
 
     }
@@ -25,9 +25,9 @@ export class ListActorComponent implements OnInit{
       const url = 'http://localhost:8080/cinema/api/actor';
       this.http.get(url).subscribe((actors: Actor[])  => {
         this.actors = actors;
-        //console.log(this.actors);
+        // console.log(this.actors);
       });
-    }  
+    }
 }
 
 
